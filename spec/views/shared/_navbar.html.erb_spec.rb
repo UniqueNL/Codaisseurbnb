7292073e497870911bb656c5ae3a8116d600c00f
UniqueNL <samtaal@outlook.com>
2016-10-12 11:53:14 +0200

    Add Feature Specs: Rooms

--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -1,25 +1,41 @@
 require 'rails_helper'
 
 describe "shared/_navbar.html.erb" do
-  before { sign_in user }
+  context "for signed in users" do
+    before { sign_in user }
 
-  context "without profile" do
-    let(:user) { create :user }
+    context "without profile" do
+      let(:user) { create :user }
 
-    it "renders email" do
-      render
-      expect(rendered).to have_content user.email
+      it "renders email" do
+        render
+        expect(rendered).to have_content user.email
+      end
+    end
+
+    context "with profile" do
+      let(:profile) { create :profile }
+      let(:user) { create :user, profile: profile }
+
+      it "renders full name" do
+        render
+        expect(rendered).to have_content profile.first_name
+        expect(rendered).to have_content profile.last_name
+      end
+    end
+
+  end
+
+  context "for anonymous users" do
+
+    context "check logged in" do
+
+      it "render sign up and login" do
+        render
+        expect(rendered).to have_content("Log In")
+        expect(rendered).to have_content("Sign Up")
+      end
     end
   end
 
-  context "with profile" do
-    let(:profile) { create :profile }
-    let(:user) { create :user, profile: profile }
-
-    it "renders full name" do
-      render
-      expect(rendered).to have_content profile.first_name
-      expect(rendered).to have_content profile.last_name
-    end
-  end
 end
